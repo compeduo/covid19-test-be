d0439cd6c24cade3ba68f8dd49131515688190e0
Dacos Nguyen <dacos.nguyen@gmail.com>
2020-03-22 23:15:54 +0100

    ddl sql updated.

--- form.sql
+++ form.sql
@@ -2,10 +2,10 @@
 -- version 3.5.8.2
 -- http://www.phpmyadmin.net
 --
--- Počítač: md42.wedos.net:3306
--- Vygenerováno: Stř 18. bře 2020, 19:42
--- Verze serveru: 10.1.37-MariaDB
--- Verze PHP: 5.4.23
+-- Host: md42.wedos.net:3306
+-- Generation Time: Mar 22, 2020 at 11:15 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.4.23
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,27 +17,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databáze: `d215516_covid`
+-- Database: `d215516_covid`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `form`
+-- Table structure for table `form`
 --
 
 CREATE TABLE IF NOT EXISTS `form` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `surname` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `age` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `phone` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `email` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `street` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `zip` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  `city` varchar(200) COLLATE utf8_czech_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+                                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                                      `phone` varchar(20) COLLATE utf8_czech_ci NOT NULL,
+                                      `fever` tinyint(1) NOT NULL,
+                                      `long_fever` tinyint(1) NOT NULL,
+                                      `breathing` tinyint(1) NOT NULL,
+                                      `cough` tinyint(1) NOT NULL,
+                                      `old` tinyint(1) NOT NULL,
+                                      PRIMARY KEY (`id`),
+                                      KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `form`
+--
+
+INSERT INTO `form` (`id`, `phone`, `fever`, `long_fever`, `breathing`, `cough`, `old`) VALUES
+(1, '608747877', 0, 0, 0, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
