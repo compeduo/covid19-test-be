17199b4ba1b5704ea26c2a65748cb6d8ad8d8a7a
Dacos Nguyen <dacos.nguyen@gmail.com>
2020-03-23 23:46:00 +0100

    Added Access-Control-Allow-x.

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 include "./functions.php";
 $form = new Form();
 
+header("Access-Control-Allow-Origin: https://covid19-test.now.sh");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
 $requestMethod = $_SERVER["REQUEST_METHOD"];
+if ($requestMethod == "OPTIONS") {
+    http_response_code(200);
+    exit(0);
+}
 if ($requestMethod != "POST") {
     http_response_code(400);
     die("Not a POST method");
@@ -21,7 +29,7 @@ if ($data == null) {
     die("Data are null!");
 }
 
-echo "Processing:\n";
+echo "Processing: \n";
 print_r($content);
 
 if ($form->save($data['phone'], $data['fever'], $data['longFever'], $data['breathing'], $data['cough'], $data['old'])) {
